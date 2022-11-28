f931d4144c30f4832cbfaf3946f635c66463b3c9
Kurtis Kwan <fpsgaaa@gmail.com>
2022-11-27 22:30:23 -0800

    person stuff

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
@@ -1,11 +1,14 @@
 package com.nighthawk.spring_portfolio.mvc.person;
 
+import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.GregorianCalendar;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -76,6 +79,9 @@ public class Person {
     @Type(type="json")
     @Column(columnDefinition = "jsonb")
     private Map<String,Map<String, Object>> stats = new HashMap<>(); 
+
+    // ArrayList of stats
+    private ArrayList<Map<String,Map<String, Object>>> statArray = new ArrayList<Map<String,Map<String, Object>>>();
     
 
     // Constructor used when building object from an API
@@ -93,5 +99,19 @@ public class Person {
             return Period.between(birthDay, LocalDate.now()).getYears(); }
         return -1;
     }
+
+    // main class as a tester
+    public static void main(String[] args) {
+        // Person empty object
+        Person p1 = new Person();
+
+        // using gregorian calendar to initialize tester date object
+        Date dob2 = new GregorianCalendar(1996, 10, 20).getTime();
+        Person p2 = new Person("jimmycricket@gmail.com", "jimmy", "Jimmy Bong", dob2);
+        
+        
+        System.out.println(p1);
+        System.out.println(p2);
+     }
 
 }
